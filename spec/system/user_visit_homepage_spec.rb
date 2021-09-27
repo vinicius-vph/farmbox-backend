12fdf3afdace23cfd5dc98ef489fabaeafebc1e1
Vinicius Santos <vinicius.vph@gmail.com>
2021-09-27 00:37:27 -0300

    created homepage

--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -7,5 +7,17 @@ describe 'User visit homepage' do
 
       expect(current_path).to eq(root_path)
     end
+    it 'Should be able view nav content' do
+      visit root_path
+
+      expect(page).to have_selector(:css, 'nav img#nav-logo')
+      expect(page).to have_link('Entrar')
+    end
+    it 'Should be able view body content' do
+      visit root_path
+
+      expect(page).to have_text('Bem vindo ao')
+      expect(page).to have_selector(:css, 'section img.flex')
+    end
   end
 end
